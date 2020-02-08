e1b773ecf35b4ecf480a25166f07241182e75786
Daniel Salazar <dnlsalazar27@gmail.com>
2020-02-07 23:27:22 -0400

    Added full animations on end of scroll

--- ElementsScrollAnimation.js
+++ ElementsScrollAnimation.js
@@ -1,5 +1,6 @@
 
 var animable = document.getElementsByClassName("myAnimatable");
+var body = document.getElementsByTagName("body")[0]
 
 window.addEventListener("load", function () {
     var items = animable.length;
@@ -102,8 +103,13 @@ function animate (data) {
 }
 
 function animateScroll (e) {
+    const bodyHeight = body.getBoundingClientRect().height
+    const scrolled = window.scrollY
+
+    console.log("Scrolled: " + scrolled + "    bodyHeight: " + bodyHeight)
     for(var i = 0; i < animable.length; i++) {
-        if(animable[i].getBoundingClientRect().top < 600) {
+        if(animable[i].getBoundingClientRect().top < 600 || scrolled >= bodyHeight) {
+            // console.log(animable[i].getBoundingClientRect())
             animable[i].addEventListener("click", animate);
             animable[i].addEventListener("animationend", stopAnimation);
             animate(animable[i]);
